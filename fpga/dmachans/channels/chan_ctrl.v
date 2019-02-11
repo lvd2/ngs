f30ff2da41691f385617849f8cc91cbc5cac69de
lvd <lvd@2c12ffe6-1fdb-4dd0-bd7a-bcf4bd823e20>
2019-02-11 12:23:19 +0000

    dmachans: verilog
    git-svn-id: svn://localhost/nedopc/ngs@150 2c12ffe6-1fdb-4dd0-bd7a-bcf4bd823e20

--- fpga/dmachans/channels/chan_ctrl.v
+++ fpga/dmachans/channels/chan_ctrl.v
@@ -13,7 +13,7 @@ module chan_ctrl
 	output reg  [ 6:0] rd_addr,
 	input  wire [31:0] rd_data,
 	//
-	output wire [ 6:0] wr_addr,
+	output reg  [ 6:0] wr_addr,
 	output wire [31:0] wr_data,
 	output wire        wr_stb,
 
@@ -37,8 +37,27 @@ module chan_ctrl
 	reg [3:0] st;
 	reg [3:0] next_st;
 
-	// enable
+	// channel enable
 	wire ch_ena = ch_enas[curr_ch[4:0]];
+
+	// offset storage
+	reg [31:0] offset;
+	reg        off_cy; // extra carry [32th bit]
+
+	// offset>=size flag
+	reg oversize;
+
+	// volumes storage
+	reg [5:0] vol_left;
+	reg [5:0] vol_right;
+	// miscellaneous
+	reg loopena;
+	reg surround;
+	
+	// base address
+	reg [13:0] base;
+
+
 
 
 	always @(posedge clk)
@@ -51,12 +70,12 @@ module chan_ctrl
 
 
 
-	localparam ST_BEGIN   = 4'd0;
-	localparam ST_GETOFFS = 4'd1;
-	//localparam ST_ = 4'd;
-	//localparam ST_ = 4'd;
-	//localparam ST_ = 4'd;
-	//localparam ST_ = 4'd;
+	localparam ST_BEGIN     = 4'd0;
+	localparam ST_GETOFFS   = 4'd1; // when offset value arrives
+	localparam ST_GETADDVOL = 4'd2; // whed add and volumes arrive
+	localparam ST_GETSIZE   = 4'd3; // size and part of base address arrive
+	localparam ST_GETLOOP   = 4'd4; // when loop and last part of base address arrive
+	localparam ST_SAVEOFFS  = 4'd5;
 	//localparam ST_ = 4'd;
 	//localparam ST_ = 4'd;
 	//localparam ST_ = 4'd;
@@ -83,10 +102,18 @@ module chan_ctrl
 		next_st = ST_GETOFFS;
 	///////////////////////////////////////////////////////////////////////
 	ST_GETOFFS:
+		next_st = ST_GETADDVOL;
 	///////////////////////////////////////////////////////////////////////
+	ST_GETADDVOL:
+		next_st = ST_GETSIZE;
 	///////////////////////////////////////////////////////////////////////
+	ST_GETSIZE:
+		next_st = ST_GETLOOP;
 	///////////////////////////////////////////////////////////////////////
+	ST_GETLOOP:
+		next_st = ST_SAVEOFFS;
 	///////////////////////////////////////////////////////////////////////
+	ST_SAVEOFFS:
 	///////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////
@@ -99,7 +126,8 @@ module chan_ctrl
 		next_st = ST_BEGIN;
 	else
 		next_st = ST_WAIT;
-	///////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////
+	///////////////////////
 	default: next_st = ST_WAIT;
 	///////////////////////////////////////////////////////////////////////
 	endcase
@@ -108,16 +136,61 @@ module chan_ctrl
 	// state memory address control
 	always @*
 		rd_addr[6:2] <= curr_ch[4:0];
+	always @*
+		wr_addr[6:2] <= curr_ch[4:0];
+	always @(posedge clk)
+		wr_addr[1:0] <= 2'd0;
 	//
 	always @(posedge clk)
 	if( st==ST_NEXT )
 	begin
 		rd_addr[1:0] <= 2'd0;
 	end
-	else if( st==ST_BEGIN )
+	else if( st==ST_BEGIN || st==ST_GETOFFS )
 	begin
 		rd_addr[1:0] <= rd_addr[1:0] + 2'd1;
 	end
+
+
+
+	// offset register control
+	always @(posedge clk)
+	if( st==ST_GETOFFS )
+		offset <= rd_data;
+	else if( st==ST_GETADDVOL )
+		{off_cy, offset} <= {1'b0, offset} + {1'b0, 14'd0, rd_data[31:14]};
+	else if( st==ST_GETLOOP )
+		offset[31:12] <= oversize ? (offset[31:12]+rd_data[27:8]) : offset[31:12]; // TODO: or maybe rd_data & {20{oversize}} ?
+
+	// offset overflow control
+	always @(posedge clk)
+	if( st==ST_GETSIZE )
+		oversize <= ( {off_cy,offset[31:12]} >= {1'b0, rd_data[27:8]} );
+
+
+
+	// volumes and miscellaneous
+	always @(posedge clk)
+	if( st==ST_GETADDVOL )
+	begin
+		vol_left  <= rd_data[11:6];
+		vol_right <= rd_data[ 5:0];
+
+		loopena  <= rd_data[13];
+		surround <= rd_data[12];
+	end
+
+
+
+	// base address
+	always @(posedge clk)
+	if( st==ST_GETSIZE )
+		base[13:8] <= rd_data[7:0];
+	//
+	always @(posedge clk)
+	if( st==ST_GETLOOP )
+		base[7:0] <= rd_data[7:0];
+
 
 
 
